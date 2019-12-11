4516b17cca457de2d0c29957487296b75601c6b6
Mariano Montone <marianomontone@gmail.com>
2019-12-11 10:25:15 -0300

    Use cl-json

--- odata.lisp
+++ odata.lisp
@@ -4,3 +4,14 @@
 
 (defparameter +trip-pin+ "https://services.odata.org/V4/TripPinServiceRW")
 
+(push '("application" . "json") drakma:*text-content-types*)
+
+(drakma:http-request +trip-pin+)
+
+(defparameter +trip-pin-modify+
+  (multiple-value-bind (response status headers modify-url)
+      (drakma:http-request +trip-pin+)
+    modify-url))
+
+(json:decode-json-from-string
+ (drakma:http-request +trip-pin-modify+))
