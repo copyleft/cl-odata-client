78863e3c89d4cf6786616aebebee5bcd3b08fae7
Mariano Montone <marianomontone@gmail.com>
2019-12-11 13:23:00 -0300

    First XML parsing. Enums generation.
    issue204650

--- odata.asd
+++ odata.asd
@@ -8,4 +8,4 @@
   :serial t
   :components ((:file "package")
                (:file "odata"))
-  :depends-on (:cxml :drakma :cl-arrows :cl-json :access))
+  :depends-on (:cxml :drakma :cl-arrows :cl-json :access :defenum))
--- odata.lisp
+++ odata.lisp
@@ -32,3 +32,30 @@
 
 (with-odata-base +trip-pin-modify+
   (odata-get* "#Person"))
+
+(defparameter +msgraph-metadata+
+  (cxml:parse (asdf:system-relative-pathname :odata "msgraph.xml")
+              (cxml-dom:make-dom-builder)))
+
+(defun child-node (name node)
+  (find-if (lambda (nd)
+             (string= (dom:node-name nd) name))
+           (dom:child-nodes node)))
+
+(defun generate-odata-enum (node)
+  `(defenum:defenum ,(intern (json:camel-case-to-lisp (dom:get-attribute node "Name")))
+       ,(loop
+            for child across (dom:child-nodes node)
+            when (string= (dom:node-name child) "Member")
+           collect (list (intern (json:camel-case-to-lisp
+                                  (concatenate 'string (dom:get-attribute node "Name")
+                                               (dom:get-attribute child "Name"))))
+                          (dom:get-attribute child "Value")))))
+
+(let* ((edmx (dom:document-element +msgraph-metadata+))
+       (data-services (child-node "edmx:DataServices" edmx))
+       (schema (child-node "Schema" data-services))
+       (enums (remove-if-not (lambda (node) (string= (dom:node-name node) "EnumType"))
+                             (dom:child-nodes schema))))
+  (loop for enum across enums
+       collect (generate-odata-enum enum)))
