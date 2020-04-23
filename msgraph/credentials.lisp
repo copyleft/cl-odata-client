3319147419e55c481b21d0cc05966e507412acb0
Mariano Montone <marianomontone@gmail.com>
2020-04-23 14:59:05 -0300

    Remove credentials

--- msgraph/credentials.lisp
+++ msgraph/credentials.lisp
@@ -1,7 +1,7 @@
 (in-package :msgraph)
 
 (defparameter *credentials*
-  (list :appid "cf9b8972-1696-4648-8746-6f6c7124d59c"
-        :tenantid "39a98eb5-7f80-43bd-b11b-999d5b3571b4"
-        :appname  "cls test"
-        :client-secret "MYfjBU1.EpYaM7imliy-sypLBlRv=65="))
+  (list :appid "appid"
+        :tenantid "tenantid"
+        :appname  "myapp"
+        :client-secret "client-secret"))
